fd561477c0e8073ccfb82bb9fc9c903d291be5ae
Saikumar <karu3570@gmail.com>
2025-07-01 12:36:16 +0530

    updated prisma

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-      experimental: {
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  experimental: {
     serverActions: true,
   },
-  };
-  
-  export default nextConfig;
-  
+};
+
+export default nextConfig;
