315d9e70e047d3b7f112d2db81b90a18822bd1d3
Saikumar <karu3570@gmail.com>
2025-07-01 21:26:31 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+   experimental: {
+    serverActions: {}, 
+  },
   images: {
     domains: ['lh3.googleusercontent.com'],
   }
