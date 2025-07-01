d2f3c50a06857a4b8dd4b86e7eb35324cc098b8a
Saikumar <karu3570@gmail.com>
2025-07-01 13:05:36 +0530

    Manually added Tailwind and PostCSS configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
+export default {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
-export default config;
+}
